d45f4dd256d5c51edf350fb2244a97aa4a345e08
Yixin Gong <3258859094@qq.com>
2023-04-19 22:01:35 +0800

    feat(parser): add basic method of parser

--- parser/ContetsFileParser.h
+++ parser/ContetsFileParser.h
@@ -2,3 +2,29 @@
 // Created by eric on 4/18/23.
 //
 
+#include "ContestFileDataBase.h"
+
+namespace ContestFileParser {
+    class ContestFileParser {
+    public:
+        void parseContestFile(const string &filename);
+
+        void getTechInfo();
+
+        void getLibCellInfo();
+
+        void getPinInfo();
+
+        void getDieInfo();
+
+        void getTerminalInfo();
+
+        void getInstInfo();
+
+        void getInstCellInfo();
+
+        void getInstNetInfo();
+
+        void getNetPinInfo();
+    };
+}
