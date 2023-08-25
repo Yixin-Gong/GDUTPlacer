7ac36fcf49c0fcd58888f6ae442882779ec53acd
Eric Gong <109069098+Yixin-Gong@users.noreply.github.com>
2023-08-25 08:32:31 +0000

    feat(database):Implemented getter and setter in die.h and lib_cell.h

--- GPlace/database/die.h
+++ GPlace/database/die.h
@@ -8,21 +8,21 @@
 #include <iostream>
 #include <array>
 
-class Die {
- public:
-  Die() {
+class Die
+{
+public:
+  Die()
+  {
     xl_ = 0;
     xh_ = 0;
     yl_ = 0;
     yh_ = 0;
-
     start_xy_[0] = 0;
     start_xy_[1] = 0;
-
     row_lh_[0] = 0;
     row_lh_[1] = 0;
-
     repeat_count_ = 0;
+    tech_name_ = "";
   }
 
   void set_size(const int32_t &xl, const int32_t &xh, const int32_t &yl, const int32_t &yh);
@@ -34,22 +34,35 @@ class Die {
   void set_util(const int32_t &util);
   void set_tech(const std::string &tech);
 
-  int32_t &get_repeat() { return repeat_count_; }
-  int32_t &get_util() { return this->max_util_; }
+  const int32_t xl() const { return xl_; }
+  int32_t xl() { return xl_; }
+  const int32_t xh() const { return xh_; }
+  int32_t xh() { return xh_; }
+  const int32_t yl() const { return yl_; }
+  int32_t yl() { return yl_; }
+  const int32_t yh() const { return yh_; }
+  int32_t yh() { return yh_; }
+  const int32_t get_util() const { return this->max_util_; }
+  int32_t get_util() { return this->max_util_; }
+  const std::array<int32_t, 2> &start_xy() const { return start_xy_; }
+  std::array<int32_t, 2> &start_xy() { return start_xy_; }
+  const std::array<int32_t, 2> &row_lh() const { return row_lh_; }
+  std::array<int32_t, 2> &row_lh() { return row_lh_; }
+  const int32_t get_repeat() const { return this->repeat_count_; }
+  int32_t get_repeat() { return this->repeat_count_; }
+  const std::string &get_tech() const { return this->tech_name_; }
   std::string &get_tech() { return this->tech_name_; }
- private:
+
+private:
   int32_t xl_{};
   int32_t xh_{};
   int32_t yl_{};
   int32_t yh_{};
-
   int32_t max_util_{};
-
   std::array<int32_t, 2> start_xy_{};
   std::array<int32_t, 2> row_lh_{};
   int32_t repeat_count_{};
-
   std::string tech_name_{};
 };
 
-#endif //VIOLET_GPLACE_DATABASE_DIE_H_
+#endif // VIOLET_GPLACE_DATABASE_DIE_H_
--- GPlace/database/lib_cell.h
+++ GPlace/database/lib_cell.h
@@ -1,31 +1,38 @@
-#ifndef  VIOLET_GPLACE_DATABASE_LIB_CELL_H_
-#define  VIOLET_GPLACE_DATABASE_LIB_CELL_H_
+#ifndef VIOLET_GPLACE_DATABASE_LIB_CELL_H_
+#define VIOLET_GPLACE_DATABASE_LIB_CELL_H_
 
 #include <iostream>
 #include <memory>
 #include <array>
 #include <unordered_map>
 #include "database/lib_pin.h"
-class LibCell {
- public:
-  LibCell() {
+class LibCell
+{
+public:
+  LibCell()
+  {
     size_[0] = 0;
     size_[1] = 0;
-    macro_ = 0;
+    is_macro_ = 0;
   }
-  const std::unordered_map<std::string, LibPin> &pins() { return pins_; }
-  uint32_t &pin_num() { return pin_num_; }
-
-  void set_size(const int32_t &l, const int32_t &w);
-  void set_pin_num(const uint32_t &num) { this->pin_num_ = num;};
+  const std::unordered_map<std::string, LibPin> &pins() const { return pins_; }
+  std::unordered_map<std::string, LibPin> &pins() { return pins_; }
+  const std::array<int32_t, 2> &size() const { return size_; }
+  std::array<int32_t, 2> &size() { return size_; }
+  const void set_size(const int32_t &l, const int32_t &w);
+  
+  void set_pin_num(const uint32_t &num) { this->pin_num_ = num; };
   void add_pin(const std::string &name, LibPin &lib_pin);
-  void is_macro() { this->macro_ = 1; }
+  void is_macro() { this->is_macro_ = 1; }
+  
   std::array<double, 2> get_center();
- protected:
+  uint32_t pin_num() { return pin_num_; }
+
+protected:
   std::unordered_map<std::string, LibPin> pins_{};
   std::array<int32_t, 2> size_{};
   uint32_t pin_num_{};
-  uint32_t macro_{};
+  uint32_t is_macro_{};
 };
 
-#endif //VIOLET_GPLACE_DATABASE_LIB_CELL_H_
+#endif // VIOLET_GPLACE_DATABASE_LIB_CELL_H_
